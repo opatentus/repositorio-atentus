8fa14d429970c2e8030916e4491fe6b32d572a66
Oscar Fabian <oparra@atentus.com>
2018-04-26 17:09:58 -0300

    Primer commit realizado el 26 de abril de 2018

---
+++ cuadro.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+	<link rel="stylesheet" href="css/estilos.css"  type="text/css">	
+
+	</title>
+</head>
+<body>
+
+	<h3> Bienvenido al programa </h3>
+
+	<h4>    Formulario de ingreso                  </h4>
+
+	<form action="#"  method="post">
+    	<label for="nombre"> Nombre </label>
+    	<br>
+    	<input type="text" class="persona" name="nombre" placeholder="Digite su nombre">
+    	<br>
+    	<label for="apellido"> Apellido </label>
+    	<br>
+    	<input type="text" class="persona" name="apellido" placeholder="Digite su apellido">
+    	<br>
+    	<label for="correo"> Correo </label>
+    	<br>
+    	<input type="text" class="persona" name="correo" placeholder="Digite su correo">
+    	<br>
+    	<label for="usuario"> Usuario </label>
+    	<br>
+    	<input type="text" class="login" name="usuario" placeholder="Digite su usario">
+    	<br>
+    	<label for="clave"> Password </label>
+    	<br>
+    	<input type="password" class="login" name="clave" placeholder="Digite su clave">
+    	<br>
+	</form>	
+</body>
+</html>
